e6feb1f1c0e4077ba527816c5abca61d7c66af01
Andrew Dawes <dawes@pacificu.edu>
2016-03-28 15:09:14 -0700

    fixed scrub_sols to use new jupyter libraries

--- scrub_sols.py
+++ scrub_sols.py
@@ -9,34 +9,35 @@ import io
 import os
 import sys
 
-from IPython.nbformat.current import read, write
+from nbformat import read, write, NO_CONVERT
 
 def scrub_code_cells(nb):
     scrubbed = 0
     cells = 0
-    for ws in nb.worksheets:
-        for cell in ws.cells:
-            if cell.cell_type != 'code':
-                continue
-            cells += 1
-            # scrub cells marked with initial '# Solution' comment
-            # any other marker will do, or it could be unconditional
-            if cell.input.startswith("# Solution"):
-                cell.input = u'# Solution goes here'
-                scrubbed += 1
-                cell.outputs = []
+    #for ws in nb.worksheets:
+    #    for cell in ws.cells:
+    for cell in nb.cells:
+        if cell.cell_type != 'code':
+            continue
+        cells += 1
+        # scrub cells marked with initial '# Solution' comment
+        # any other marker will do, or it could be unconditional
+        if cell.source.startswith("# Solution"):
+            cell.source = u'# Solution goes here'
+            scrubbed += 1
+            cell.outputs = []
 
     print
-    print("scrubbed %i/%i code cells from notebook %s" % (scrubbed, cells, nb.metadata.name))
+    print("scrubbed %i/%i code cells from notebook" % (scrubbed, cells))
 
 if __name__ == '__main__':
     for ipynb in sys.argv[1:]:
         print("scrubbing %s" % ipynb)
         with io.open(ipynb, encoding='utf8') as f:
-            nb = read(f, 'json')
+            nb = read(f, NO_CONVERT)
         scrub_code_cells(nb)
         base, ext = os.path.splitext(ipynb)
         new_ipynb = "%s_blank%s" % (base, ext)
         with io.open(new_ipynb, 'w', encoding='utf8') as f:
-            write(nb, f, 'json')
+            write(nb, f)
         print("wrote %s" % new_ipynb)
